6c31732e6c3d00316b390deb7af07b7f270b64cc
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-03-15 05:30:43 -0400

    Implemented new state definition

--- voronoi.rkt
+++ voronoi.rkt
@@ -3,8 +3,14 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
+;; These are the world states
+;; The world will be a before before the program starts running the algorithm
+;; The world will be a working while the algorithm is running
+
 ;; A state is: (state (list-of (pair x y)))
-(struct state (points))
+;; A working is (working (list-of (pair x y)))
+(struct before (points))
+(struct working (points))
 
 (define START-BUTTON (overlay (text "START" 22 'black) (rectangle 100 50 'solid 'PaleTurquoise)))
 
@@ -12,14 +18,17 @@
 (define (render s)
   (above (foldr (lambda (v im) (place-image (circle 3 'solid 'red) (car v) (cdr v) im))
                 (empty-scene 900 600)
-                (state-points s))
+                (before-points s))
          START-BUTTON))
 
 (define (add-point last-state x y event)
   (if (equal? event "button-down")
-  (struct-copy state last-state [points (cons (cons x y) (state-points last-state))])
+  (struct-copy before last-state [points (cons (cons x y) (before-points last-state))])
   last-state))
 
-(define (begin) (big-bang (state '())
-                          (on-mouse add-point)
+(define (begin) (big-bang (before '())
+                          (on-mouse (lambda (st x y event) (if
+                                                            (before? st)
+                                                            (add-point st x y event)
+                                                            st)))
                           (to-draw render)))
