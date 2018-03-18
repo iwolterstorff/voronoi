e3d8f6ab7ec310ea9d2f8421f89b1b43ad54884f
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2018-03-17 21:52:18 -0400

    Added to readme and added tick handler

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # voronoi
 
 An implementation of Fortune's algorithm for contructing Voronoi diagrams (built in Racket for the memez)
+(Also this is a work in progress so don't judge me by it yet)
--- voronoi.rkt
+++ voronoi.rkt
@@ -4,13 +4,14 @@
 (require 2htdp/universe)
 
 ;; These are the world states
+;; A state is (before U working)
 ;; The world will be a before before the program starts running the algorithm
 ;; The world will be a working while the algorithm is running
 
-;; A state is: (state (list-of (pair x y)))
-;; A working is (working (list-of (pair x y)))
+;; A state is: (before (list-of (pair x y)))
+;; A working is (working (list-of (pair x y)) number)
 (struct before (points))
-(struct working (points))
+(struct working (points sweep))
 
 (define START-BUTTON (overlay (text "START" 22 'black) (rectangle 100 50 'solid 'PaleTurquoise)))
 
@@ -26,9 +27,19 @@
   (struct-copy before last-state [points (cons (cons x y) (before-points last-state))])
   last-state))
 
+(define (tick last-state)
+  (cond
+    [(before? last-state) last-state]
+    [(working? last-state) last-state]
+    [else last-state]))
+
 (define (begin) (big-bang (before '())
                           (on-mouse (lambda (st x y event) (if
                                                             (before? st)
                                                             (add-point st x y event)
                                                             st)))
-                          (to-draw render)))
+                          (to-draw render)
+                          (on-tick tick 0.25)))
+
+
+(begin)
